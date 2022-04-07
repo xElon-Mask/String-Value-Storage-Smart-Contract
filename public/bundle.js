33281a404d29ddba664be29f054185d3aae5ce80
xElon-Mask <xelonmask@protonmail.com>
2022-04-07 17:29:35 +0200

    deploy SC on truffle local blockchain / add ABI and address on bundle.js

--- public/bundle.js
+++ public/bundle.js
@@ -1,7 +1,51 @@
-var simpleStorageABI = [];
-var simpleStorageAddress = '';
-var web3 = new Web3('http://localhost:9545');
-var simpleStorage = new web3.eth.Contract(simpleStorageABI, simpleStorageAddress);
+const stringStorageABI = [
+  {
+    inputs: [],
+    name: "phrase",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+    constant: true,
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_phrase",
+        type: "string",
+      },
+    ],
+    name: "majPhrase",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getPhrase",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+    constant: true,
+  },
+];
+const stringStorageAddress = " 0x0225a4a155e519396C409189f16295f21288ED26";
+const web3 = new Web3("http://localhost:9545");
+const stringStorage = new web3.eth.Contract(
+  stringStorageABI,
+  stringStorageAddress
+);
 
-document.addEventListener('DOMContentLoaded', () => {
-});
+document.addEventListener("DOMContentLoaded", () => {});
